cc34525726c57c58b88cadd0dc0ccdabd613cb0f
yeongeun11 <yeongeunim322@gmail.com>
2025-09-16 09:52:35 +0900

    회원가입 폼 수정

--- 07/board/member_form.php
+++ 07/board/member_form.php
@@ -4,103 +4,83 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>회원가입</title>
+    <link rel="stylesheet" type="text/css" href="./css/common.css">
+    <link rel="stylesheet" type="text/css" href="./css/member.css">
 </head>
 
 <body>
-    <link rel="stylesheet" type="text/css" href="./css/common.css">
-    <link rel="stylesheet" type="text/css" href="./css/member.css">
-
     <script>
         // 입력 데이터 검사 함수
         function check_input() {
-            // id 입력 확인
             if (!document.member_form.id.value) {
                 alert("아이디를 입력하세요.");
-                document.member_form.id.focus(); // 입력창에 커서 이동
+                document.member_form.id.focus();
                 return;
             }
-            // pass 입력 확인
             if (!document.member_form.pass.value) {
                 alert("비밀번호를 입력하세요.");
                 document.member_form.pass.focus();
                 return;
             }
-            // pass 확인 
             if (!document.member_form.pass_confirm.value) {
                 alert("비밀번호를 확인하세요.");
                 document.member_form.pass_confirm.focus();
                 return;
             }
-            // 이름 입력 확인
             if (!document.member_form.name.value) {
                 alert("이름을 입력하세요.");
                 document.member_form.name.focus();
                 return;
             }
-            // 이메일 앞부분 입력 확인
-            if (!document.member_form.email.value) {
-                alert("이메일 주소를 입력하세요");
-                document.member_form.email.focus();
+            if (!document.member_form.email1.value) {
+                alert("이메일 주소를 입력하세요.");
+                document.member_form.email1.focus();
                 return;
             }
-            // 이메일 뒷부분 입력 확인
             if (!document.member_form.email2.value) {
-                alert("이메일 주소를 입력하세요");
+                alert("이메일 주소를 입력하세요.");
                 document.member_form.email2.focus();
                 return;
             }
 
-            // 비밀번호가 일치하지 않을 때
             if (document.member_form.pass.value != document.member_form.pass_confirm.value) {
-                alert("비밀번호가 일치하지 않습니다 \n 다시 입력해 주세요.");
+                alert("비밀번호가 일치하지 않습니다.\n다시 입력해 주세요.");
                 document.member_form.pass.focus();
                 document.member_form.pass.select();
                 return;
             }
 
-            // 모든 조건 통과 시 form 전송
             document.member_form.submit();
         }
 
-        // 입력값 초기화 함수 (취소하기 버튼)
+        // 입력값 초기화 함수
         function reset_form() {
             document.member_form.id.value = "";
             document.member_form.pass.value = "";
             document.member_form.pass_confirm.value = "";
             document.member_form.name.value = "";
-            document.member_form.email.value = "";
+            document.member_form.email1.value = "";
             document.member_form.email2.value = "";
             document.member_form.id.focus();
-            return;
-        }
-
-        function check_id() {
-            window.open("member_check_id.php?id=" + document.member_form.id.value,
-                "IDcheck",
-                "left=700,top=300,width=350,height=200,scrollbars=no,resizable=yes");
         }
     </script>
+
     <div id="join_box">
-        <!-- form name="member_form" : script에서 참조할 폼 객체 -->
-        <!-- action="member_insert.php" : 입력값 전송될 PHP 파일 -->
         <form name="member_form" method="post" action="member_insert.php">
             <h2>회원 가입</h2>
 
-            <!-- 아이디 입력 + 중복확인 버튼 -->
+            <!-- 아이디 -->
             <div class="form id">
                 <div class="col1">아이디</div>
                 <div class="col2">
                     <input type="text" name="id">
                 </div>
-                <div class="col3">
-                    <a href="#"><img src="./img/check_id.gif" onclick="check_id()"></a>
-                </div>
             </div>
 
             <div class="clear"></div>
 
-            <!-- 비밀번호 입력 -->
+            <!-- 비밀번호 -->
             <div class="form">
                 <div class="col1">비밀번호</div>
                 <div class="col2">
@@ -120,7 +100,7 @@
 
             <div class="clear"></div>
 
-            <!--이름 입력-->
+            <!-- 이름 -->
             <div class="form">
                 <div class="col1">이름</div>
                 <div class="col2">
@@ -130,11 +110,11 @@
 
             <div class="clear"></div>
 
-            <!-- 이메일 입력 -->
-            <div class="form email">
+            <!-- 이메일 -->
+            <div class="form email1">
                 <div class="col1">이메일</div>
                 <div class="col2">
-                    <input type="text" name="email">@<input type="text" name="email2">
+                    <input type="text" name="email1">@<input type="text" name="email2">
                 </div>
             </div>
 
@@ -142,15 +122,10 @@
 
             <div class="bottom_line"></div>
 
-            <!-- 저장, 취소 버튼 -->
+            <!-- 버튼 -->
             <div class="buttons">
-                <!-- 저장 버튼: check_input() 실행 -->
-                <img style="cursor:pointer" onclick="check_input()"
-                    src="./img/button_save.gif">&nbsp;
-
-                <!-- 취소 버튼: reset_form() 실행 -->
-                <img id="reset_button" style="cursor:pointer"
-                    src="./img/button_reset.gif" onclick="reset_form()">
+                <img style="cursor:pointer" onclick="check_input()" src="./img/button_save.gif">&nbsp;
+                <img id="reset_button" style="cursor:pointer" src="./img/button_reset.gif" onclick="reset_form()">
             </div>
         </form>
     </div>
