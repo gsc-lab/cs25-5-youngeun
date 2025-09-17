fd737e90d0612fb9facb637725de0002153a2aa1
yeongeun11 <yeongeunim322@gmail.com>
2025-09-17 23:13:13 +0900

    레이아웃 기본 구조 추가

---
+++ 07/board/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>PHP</title>
+    <link rel="stylesheet" type="text/css" href="common.css">
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+    <section>
+        <?php include "main.php"; ?>
+    </section>
+</body>
+
+</html>
