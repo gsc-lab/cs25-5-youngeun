9540ebd2a6ab31d4451725cb7a30bff0239ae413
yeongeun11 <yeongeunim322@gmail.com>
2025-09-24 18:59:17 +0900

    헤더 메뉴바 추가

--- 07/board/header.php
+++ 07/board/header.php
@@ -1,22 +1,29 @@
 ﻿<?php
+// 세션 시작
 session_start();
+
+// 세션에 저장된 값 확인 (로그인 했는지 체크)
 if (isset($_SESSION["userid"])) $userid = $_SESSION["userid"];
 else $userid = "";
 if (isset($_SESSION["username"])) $username = $_SESSION["username"];
 else $username = "";
 ?>
+
+<!--상단 로그, 메뉴-->
 <div id="top">
     <h3>
         <a href="index.php">PHP</a>
     </h3>
     <ul id="top_menu">
         <?php
+        // 로그인 안 했을 때
         if (!$userid) {
         ?>
             <li><a href="member_form.php">회원 가입</a> </li>
             <li><a href="login_form.php">로그인</a></li>
         <?php
         } else {
+            // 로그인 했을 때 -> 사용자 아이디, 이름 표시
             $logged = $username . "(" . $userid . ")님";
         ?>
             <li><?= $logged ?> </li>
@@ -34,6 +41,7 @@ else $username = "";
         ?>
     </ul>
 </div>
+<!-- 메뉴바 -->
 <div id="menu_bar">
     <ul>
         <li><a href="index.php">HOME</a></li>
