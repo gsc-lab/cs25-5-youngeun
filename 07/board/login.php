9293567c0f7acbdebde60d66f6ef96a50036858b
yeongeun11 <yeongeunim322@gmail.com>
2025-09-24 18:46:47 +0900

    로그인페이지

--- 07/board/login.php
+++ 07/board/login.php
@@ -4,26 +4,31 @@
 $id = $_POST['id'];
 $pass = $_POST['pass'];
 
+// 데이터베이스 연결
 $con = mysqli_connect("localhost", "user", "12345", "sample");
+
+// members 테이블에서 입력한 id와 일치하는 데이터 검색
 $sql = "select * from members where id='$id'";
 $result = mysqli_query($con, $sql);
-
-
 $num_match = mysqli_num_rows($result);
 
+
 if (!$num_match) {
+    // 아이디 없음
     echo ("
     <script>
     window.alert('등록되지 않은 아이디입니다.')
     history.go(-1)
     ");
 } else {
+    // 아이디 있음
     $row = mysqli_fetch_array($result);
     $db_pass = $row["pass"];
 
     mysqli_close($con);
 
     if ($pass != $db_pass) {
+        // 비밀번호 틀림
         echo ("
         <script>
         window.alert('비밀번호가 틀립니다.')
@@ -32,12 +37,14 @@ if (!$num_match) {
         );
         exit;
     } else {
+        // 로그인 성공 -> 세션 저장
         session_start();
         $_SESSION["userid"] = $row["id"];
         $_SESSION["username"] = $row["name"];
         $_SESSION["userlevel"] = $row["level"];
         $_SESSION["userpoint"] = $row["point"];
-
+        
+        // 메인으로 이동
         echo ("
               <script>
                 location.href = 'index.php';
