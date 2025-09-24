bcac858f80f64d3f33818a6cdeed158896e8e51f
yeongeun11 <yeongeunim322@gmail.com>
2025-09-24 18:55:00 +0900

    메인 화면 틀

--- 07/board/index.php
+++ 07/board/index.php
@@ -4,15 +4,18 @@
 <head>
     <meta charset="utf-8">
     <title>PHP</title>
+    <!-- CSS 불러오기 -->
     <link rel="stylesheet" type="text/css" href="common.css">
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <body>
+    <!-- header.php 파일 불러옴 -->
     <header>
         <?php include "header.php"; ?>
     </header>
     <section>
+        <!-- main.php 파일 불러옴 -->
         <?php include "main.php"; ?>
     </section>
 </body>
