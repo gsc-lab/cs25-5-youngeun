8d154a774dee6d6dea40d587018fc844cd74bf19
yeongeun11 <yeongeunim322@gmail.com>
2025-11-20 15:09:56 +0900

    로그아웃 코드

---
+++ 08/login-board1/logout.php
@@ -0,0 +1,27 @@
+<?php
+
+session_start();
+
+// 세션 데이터 제거
+$_SESSION = [];
+
+// 세션 쿠키 제거
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 3600,
+        $params['path'],
+        $params['domain'],
+        $params['secure'],
+        $params['httponly']
+    );
+}
+
+// 세션 파괴
+session_destroy();
+
+// 로그인 페이지로 이동
+header("Location: login.php");
+exit;
